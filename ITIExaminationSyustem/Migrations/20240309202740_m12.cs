da2e88e18c1d61fdab24ec3e7e030b711aef1aa8
DavidAyad97 <davidayad80@gmail.com>
2024-03-09 22:53:29 +0200

    add migration m12

---
+++ ITIExaminationSyustem/Migrations/20240309202740_m12.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITIExaminationSyustem.Migrations
+{
+    /// <inheritdoc />
+    public partial class m12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "Exam_Duration",
+                table: "Exams");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "QuestionTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Grade",
+                table: "Exams",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Course_Exam_StartDate",
+                table: "Courses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Crs_Exam_Duration",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "QuestionTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Course_Exam_StartDate",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Crs_Exam_Duration",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Grade",
+                table: "Exams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Exams",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Exam_Duration",
+                table: "Exams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
