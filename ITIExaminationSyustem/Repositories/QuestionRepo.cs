781a771724302ac2a4cb6c08741e54289e2e497e
mahmoudmowiena <mowiena8@gmail.com>
2024-03-09 20:12:21 +0200

    7 Repos had been implemented

---
+++ ITIExaminationSyustem/Repositories/QuestionRepo.cs
@@ -0,0 +1,39 @@
+﻿using ITIExaminationSyustem.Interfaces;
+using ITIExaminationSyustem.Models;
+
+namespace ITIExaminationSyustem.Repositories
+{
+    public class QuestionRepo : IQuestionRepo
+    {
+        Exam_Context _context;
+
+        public QuestionRepo(Exam_Context examContext)
+        {
+            _context = examContext;
+        }
+        public List<Question> GetAll()
+        {
+            return _context.Questions.ToList();
+        }
+        public Question GetById(int id)
+        {
+            return _context.Questions.SingleOrDefault(question => question.Question_Id == id);
+        }
+        public void Add(Question question)
+        {
+            _context.Questions.Add(question);
+            _context.SaveChanges();
+        }
+        public void Update(Question question)
+        {
+            _context.Questions.Update(question);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var questionToDelete = _context.Questions.SingleOrDefault(question => question.Question_Id == id);
+            _context.Questions.Remove(questionToDelete);
+            _context.SaveChanges();
+        }
+    }
+}
