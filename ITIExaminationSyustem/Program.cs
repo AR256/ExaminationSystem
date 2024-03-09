74c4907c5cea6e5f1d72cf58b1eef8149ff764ae
Abdulrahman <abdulrahman.rehann@gmail.com>
2024-03-09 23:18:17 +0200

    added repos Injection

--- ITIExaminationSyustem/Program.cs
+++ ITIExaminationSyustem/Program.cs
@@ -1,4 +1,6 @@
+using ITIExaminationSyustem.Interfaces;
 using ITIExaminationSyustem.Models;
+using ITIExaminationSyustem.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace ITIExaminationSyustem
@@ -15,6 +17,23 @@ namespace ITIExaminationSyustem
             {
                 a.UseSqlServer(builder.Configuration.GetConnectionString("con1"));
             });
+            builder.Services.AddScoped<IAdminRepo, AdminRepo>();
+            builder.Services.AddScoped<IBranchManagerRepo, BranchManagerRepo>();
+            builder.Services.AddScoped<IBranchRepo, BranchRepo>();
+            builder.Services.AddScoped<IChoiceRepo, ChoiceRepo>();
+            builder.Services.AddScoped<ICourseRepo, CourseRepo>();
+            builder.Services.AddScoped<IDepartmentRepo, DepartmentRepo>();
+            builder.Services.AddScoped<IDeptInstructorRepo, DeptInstructorRepo>();
+            builder.Services.AddScoped<IExamQuestionRepo, ExamQuestionRepo>();
+            builder.Services.AddScoped<IExamRepo, ExamRepo>();
+            builder.Services.AddScoped<IInstructorRepo, InstructorRepo>();
+            builder.Services.AddScoped<IMainDeptRepo, MainDeptRepo>();
+            builder.Services.AddScoped<IQuestionRepo, QuestionRepo>();
+            builder.Services.AddScoped<IQuestionTypeRepo, QuestionTypeRepo>();
+            builder.Services.AddScoped<IRoleRepo, RoleRepo>();
+            builder.Services.AddScoped<IStudentCourseRepo, StudentCourseRepo>();
+            builder.Services.AddScoped<IStudentRepo, StudentRepo>();
+            builder.Services.AddScoped<IUserRepo, UserRepo>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
