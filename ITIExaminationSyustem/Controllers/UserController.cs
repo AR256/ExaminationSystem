3782bea886904cd5567db764e9801f95f8d18cba
mahmoudmowiena <mowiena8@gmail.com>
2024-03-10 21:13:18 +0200

    UserController under development

---
+++ ITIExaminationSyustem/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using ITIExaminationSyustem.Interfaces;
+using ITIExaminationSyustem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITIExaminationSyustem.Controllers
+{
+    public class UserController : Controller
+    {
+        IUserRepo _userRepo;
+
+        public UserController(IUserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        public ActionResult Index()
+        {
+            List<User> users = _userRepo.GetAll();
+            return View(users);
+        }
+
+        public ActionResult Details(int id)
+        {
+            User fetchedUser = _userRepo.GetById(id);
+            return View(fetchedUser);
+        }
+
+    }
+}
--- ITIExaminationSyustem/Repositories/UserRepo.cs
+++ ITIExaminationSyustem/Repositories/UserRepo.cs
@@ -7,13 +7,35 @@ namespace ITIExaminationSyustem.Repositories
     public class UserRepo : IUserRepo
     {
         Exam_Context _context;
+
         public UserRepo(Exam_Context examContext)
         {
             _context = examContext;
         }
+
+        public List<User> GetAll()
+        {
+            return _context.Users.Include(user => user.Navigation_Instructor)
+                                 .Include(user => user.Navigation_Student)
+                                 .Include(user => user.Navigation_Admin)
+                                 .Include(user => user.Navigation_BranchManager)
+                                 .ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return _context.Users.Include(user => user.Navigation_Roles).SingleOrDefault(user => user.User_Id == id);
+        }
+
         public void Add(User user)
         {
             _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void Update(User user)
+        {
+            _context.Users.Update(user);
             _context.SaveChanges();
         }
 
@@ -25,16 +47,10 @@ namespace ITIExaminationSyustem.Repositories
         }
 
         //switching lists
-        public List<Role> GetRoles(int id)
-        {
-            User user = _context.Users.Include(u=>u.Navigation_Roles).SingleOrDefault(u=>u.User_Id == id);
-            return user.Navigation_Roles.ToList();
-        }
-
-        public void Update(User user)
-        {
-            _context.Users.Update(user);
-            _context.SaveChanges();
-        }
+        //public List<Role> GetRoles(int id)
+        //{
+        //    User user = _context.Users.Include(u=>u.Navigation_Roles).SingleOrDefault(u=>u.User_Id == id);
+        //    return user.Navigation_Roles.ToList();
+        //}
     }
 }
