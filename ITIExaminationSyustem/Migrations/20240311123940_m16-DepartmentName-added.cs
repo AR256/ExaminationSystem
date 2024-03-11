b3133c4b87ce4ef6620ba4f3477f77ab32c391c2
mahmoudmowiena <mowiena8@gmail.com>
2024-03-11 14:40:15 +0200

    Migartion-m16 added

---
+++ ITIExaminationSyustem/Migrations/20240311123940_m16-DepartmentName-added.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITIExaminationSyustem.Migrations
+{
+    /// <inheritdoc />
+    public partial class m16DepartmentNameadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Department_Name",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Department_Name",
+                table: "Departments");
+        }
+    }
+}
