9a104d69fc2127eb00edf1429b8d1b6e9ffc144f
DavidAyad97 <davidayad80@gmail.com>
2024-03-12 01:37:18 +0200

    remove last migration

--- ITIExaminationSyustem/ViewModels/InstructorViewModel.cs
+++ ITIExaminationSyustem/ViewModels/InstructorViewModel.cs
@@ -1,10 +1,16 @@
-﻿namespace ITIExaminationSyustem.ViewModels
+﻿using ITIExaminationSyustem.Models;
+
+namespace ITIExaminationSyustem.ViewModels
 {
     public class InstructorViewModel
     {
+        public int Branch_Id { get; set; }
         public string Instructor_Name { get; set; }
         public string Instructor_Email { get; set; }
         public string Instructor_Image { get; set; }
+        public string Instructor_BranchName { get; set; } // to be added
         public int Instructor_Id { get; set; }
+        public List<Department> Departments { get; set; }
+        public List<Course> Courses { get; set; }
     }
 }
